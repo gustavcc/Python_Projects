44288eb1f8e5a0f87bda0fd00edae497db4d75a9
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2024-01-25 23:03:45 -0300

    finished

--- Clientes Tkinter/register_clientes.py
+++ Clientes Tkinter/register_clientes.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
+import openpyxl
 
 def Connection():
     path = "clientes.db"
@@ -13,7 +14,7 @@ def Connection():
     finally:
         return c
 
-# função que define a criação do DB e da tabela
+#* função que define a criação do DB e da tabela
 # def CriarTabela():
 #     connection = Connection()
 #     try:
@@ -32,23 +33,69 @@ def Connection():
 #         print("\nErro: ",er)
 # CriarTabela()
 
+def contem_letra(str):
+    if 'a'<= str <= 'z' or 'A'<= str <= 'Z':
+        return True
+    return False
+
 #* Funções
 def cadastrar_cliente():
     connection = Connection()
     try:
+        if not Entry_nome.get() or not Entry_sobrenome.get() or not Entry_email.get() or not Entry_telefone.get():
+            erro = tk.Tk()
+            erro.title('Erro')
+            aviso1 = tk.Label(erro, text='Algum campo está errado!', foreground='red')
+            aviso1.grid(row=0, column=0, padx=20, pady=20)
+
+            aviso3 = tk.Label(erro, text='Não deixe campos vazios!', foreground='red')
+            aviso3.grid(row=1, column=0, padx=20, pady=20)
+
+            print('\nAlgum campo está errado!\n')
+        elif len(Entry_telefone.get())!=10 or contem_letra(Entry_telefone.get()) or '@' not in Entry_email.get() or '.com' not in Entry_email.get():
+            erro = tk.Tk()
+            erro.title('Erro')
+            aviso1 = tk.Label(erro, text='Algum campo está errado!', foreground='red')
+            aviso1.grid(row=0, column=0, padx=20, pady=20)
+
+            aviso2 = tk.Label(erro, text='Exemplo de telefone: 7711112222 (sem letras, 10 dígitose sem espaços) ', foreground='red')
+            aviso2.grid(row=1, column=0, padx=20, pady=20)
+
+            aviso3 = tk.Label(erro, text='Exemplo de email: exmplo@email.com (com @ e .com)', foreground='red')
+            aviso3.grid(row=2, column=0, padx=20, pady=20)
+        else:
+            c = connection.cursor()
+            c.execute(f"INSERT INTO Clientes (nome,sobrenome,telefone,email) VALUES('{Entry_nome.get()}','{Entry_sobrenome.get()}','{Entry_telefone.get()}','{Entry_email.get()}');")
+            connection.commit()
+            cadastrado = tk.Tk()
+            cadastrado.title('Erro')
+            aviso1 = tk.Label(cadastrado, text='Cliente cadastrado!', foreground='green')
+            aviso1.grid(row=0, column=0, padx=20, pady=20)
+            print("\nCliente castrado")
+            Entry_nome.delete(0, "end")
+            Entry_sobrenome.delete(0, "end")
+            Entry_email.delete(0, "end")
+            Entry_telefone.delete(0, "end")
+    except Error as er:
+        print("\nErro: ",er)
+
+def exportar_cliente():
+    connection = Connection()
+    try:
         c = connection.cursor()
-        c.execute(f"INSERT INTO Clientes (nome,sobrenome,telefone,email) VALUES('{Entry_nome.get()}','{Entry_sobrenome.get()}','{Entry_telefone.get()}','{Entry_email.get()}');")
+        c.execute(f"SELECT * FROM clientes")
+        clientes = c.fetchall()
+
+        clientes = pd.DataFrame(clientes, columns=['id','nome','sobrenome','telefone','email'])
+        clientes.to_excel('clientes.xlsx')
         connection.commit()
-        print("\nCliente castrado")
+
         Entry_nome.delete(0, "end")
         Entry_sobrenome.delete(0, "end")
         Entry_email.delete(0, "end")
         Entry_telefone.delete(0, "end")
     except Error as er:
         print("\nErro: ",er)
-
-def exportar_cliente():
-    pass
 
 #* Interface Gráfica
 window = tk.Tk()
@@ -71,9 +118,11 @@ Label_telefone.grid(row=3, column=0, padx=10, pady=10)
 
 # entrys
 Entry_nome = tk.Entry(window, text='Nome', width=30)
+Entry_nome.get().capitalize().strip()
 Entry_nome.grid(row=0, column=1, padx=10, pady=10)
 
 Entry_sobrenome = tk.Entry(window, text='Sebrenome', width=30)
+Entry_sobrenome.get().capitalize().strip()
 Entry_sobrenome.grid(row=1, column=1, padx=10, pady=10)
 
 Entry_email = tk.Entry(window, text='Email', width=30)
